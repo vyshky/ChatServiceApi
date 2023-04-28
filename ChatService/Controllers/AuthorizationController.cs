e4e34143d4fe8db0ac4ae158d44dbea6c251571b
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2023-04-28 19:06:58 +0400

    Написал получение клиентского ключа из авторизации

--- ChatService/Controllers/AuthorizationController.cs
+++ ChatService/Controllers/AuthorizationController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ChatService.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace AuthenticationService.Controllers
 {
@@ -19,19 +21,38 @@ namespace AuthenticationService.Controllers
         {
             return Ok("Success");
         } 
-        
-        
-        [HttpPost(nameof(TestServiceAuth))]
+                
+        [HttpPost(nameof(GetUserIdOutJwt))]
         [Authorize(Roles = "spd-chat-service")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public IActionResult TestServiceAuth()
+        public IActionResult GetUserIdOutJwt()
         {
             StringValues value;
-            StringValues userId;
             Request.Headers.TryGetValue("authorization", out value);
-            // TODO :: получить id авторизованного юзера из токена
-            return Ok(value);
+            string token = value[0].Replace("Bearer ", "");
+            JwtSecurityToken jwt = new JwtSecurityToken(token);
+            var userId = jwt.Claims.FirstOrDefault(x=> x.Type == "user_id").Value;
+            return Ok(userId);
         }
+
+//Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJMT0NBTCBBVVRIT1JJVFkiOiJNeUF1dGhTZXJ2ZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJ1c2VyX2lkIjoiNjQzMjQ4YzctYmEyMi00MzhmLThkOGItZTY4OTNlZGEwYTEwIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbImFkbWluIiwidXNlciIsInNwZC1jaGF0LXNlcnZpY2UiXSwiZXhwIjoxNjgyNjk1NzYwLCJpc3MiOiJNeUF1dGhTZXJ2ZXIiLCJhdWQiOiJNeUF1dGhDbGllbnQifQ.z-Hgh7LdYIPH4FWpQGHgEtKUO-dFBkmuzGN9v9VJpIY
+
+        // TODO :: получаем сообщение, получаем клиентский id, отправляем по chanelId сообщение в БД
+        // клиент должен получить оповещение о новом сообщении
+        [HttpPost(nameof(SendMessage))]
+        [Authorize(Roles = "spd-chat-service")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult SendMessage(MessageModel message)
+        {
+            StringValues value;
+            Request.Headers.TryGetValue("authorization", out value);
+            string token = value[0].Replace("Bearer ", "");
+            JwtSecurityToken jwt = new JwtSecurityToken(token);
+            var userId = jwt.Claims.FirstOrDefault(x => x.Type == "user_id").Value;
+            return Ok(userId);
+        }
+
     }
 }
---
+++ ChatService/Domain/Models/MessageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatService.Domain.Models
+{
+    public class MessageModel
+    {
+        public Guid ChanelCode { get; set; }
+        public string Content { get; set; }
+    }
+}
